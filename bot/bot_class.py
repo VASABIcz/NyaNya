01249b019338de996058214daac0b50f009a890a
VASABIcz <71564425+VASABIcz@users.noreply.github.com>
2021-12-17 22:11:25 +0100

    lot of changes

--- bot/bot_class.py
+++ bot/bot_class.py
@@ -18,20 +18,10 @@ from bot.utils.errors import *
 from bot.utils.functions_classes import intents, NyaNyaCogs
 
 
-def encod(inter):
-    table = {"0": "a", "1": "b", "2": "c", "3": "d", "4": "e", "5": "f", "6": "g", "7": "h", "8": "i", "9": "j"}
-    s = ""
-    for i in str(inter):
-        s += table[i]
-
-    return s
-
-
 class Nya_Nya(commands.AutoShardedBot):
     """
     custom shard bot class. OwO
     """
-
     def __init__(self, cfg):
         self.start_time = datetime.datetime.now()
         self.cfg = cfg
@@ -39,84 +29,76 @@ class Nya_Nya(commands.AutoShardedBot):
         self.support = cfg.SUPPORT
         self.default_emoji = cfg.DEFAULT_EMOJI
         self.session = aiohttp.ClientSession()
+        self.load_webhook()
         self.cog_manager = NyaNyaCogs(self, COGS, STATIC_COGS, IGNORED, COG_DIR)
-        # self.directory = str(Path(__file__).parent)
-        # self.loc = CodeCounter()
         self._setuped = asyncio.Event()
-        # self.loc.count(self.directory)
         self.auth_manager = SpotifyClientCredentials(client_id=self.cfg.SPOTIFY_ID,
                                                      client_secret=self.cfg.SPOTIFY_SECTRET)
         self.sp = spotipy.Spotify(auth_manager=self.auth_manager)
-        self.load_webhook()
         self.mongo_client = motor.motor_asyncio.AsyncIOMotorClient(self.cfg.MONGO)
-        self.wavelink_reload = False
         self.prefixes = aioredis.from_url(self.cfg.REDIS_PREFIXES, decode_responses=True)
+        self.wavelink_reload = False
 
         super().__init__(command_prefix=self._get_prefix,
                          intents=intents(),
                          activity=self.cfg.ACTIVITY if hasattr(self.cfg, 'ACTIVITY') else discord.Game(
                              name=f"{self.cfg.MAIN_PREFIX}help"),
-                         description="IT JUST WORKS\nwell it doesn\'t,... STFU please",
+                         description="IT JUST WORKS\nwell it doesn't,... STFU please",
                          owner_ids=self.cfg.OWNERS,
                          case_insensitive=True,
                          strip_after_prefix=True,
                          help_command=Nya_Nya_Help())
+
         self.loop.create_task(self.__ainit__())
 
     async def __ainit__(self):
         self.pdb: asyncpg.Pool = await asyncpg.create_pool(**self.cfg.DB_CREDENTIALS)
         await self.pdb.execute(sql_template.GLOBAL)  # execute initial db query
 
-        # TODO in future add this functionalyty to reaction menu
+        # TODO in future add this functionality to reaction menu
 
-        # !!! disgusting way of doing it, but im to stupid :D
         async def bok(ctx):
             self.dispatch("after_invoke", ctx)
 
         self._after_invoke = bok
 
         await self.wait_until_ready()
-        self.instance_name = encod(self.user.id)
+
+        self.instance_name = self.encod
+
         await self.pdb.execute(sql_template.INSTANCE.format(id=self.instance_name))
+
         self.invite = discord.utils.oauth_url(self.user.id, discord.Permissions(8))  # TODO change permisions etc.
         self.owner_user = self.get_user(self.owner_ids[0])
+
         self._setuped.set()
 
-        await self.log_to_db()
+        # await self.log_to_db()
 
+    @property
+    def encod(self):
+        table = {"0": "a", "1": "b", "2": "c", "3": "d", "4": "e", "5": "f", "6": "g", "7": "h", "8": "i", "9": "j"}
+        s = ""
+        for i in str(self.user.id):
+            s += table[i]
 
-    def load_webhook(self):
-        try:
-            self.error_webhook = discord.Webhook.from_url(self.cfg.ERROR_WEBHOOK_URL,
-                                                          adapter=discord.AsyncWebhookAdapter(self.session))
-        except:
-            pass
-        try:
-            self.report_webhook = discord.Webhook.from_url(self.cfg.REPORT_WEBHOOK_URL,
-                                                           adapter=discord.AsyncWebhookAdapter(self.session))
-        except:
-            pass
+        return s
 
-    async def log_to_db(self):
-        query = "INSERT INTO guilds(id) VALUES ($1)"
-        query2 = "INSERT INTO users(id, name, discriminator) VALUES ($1, $2, $3)"
-        query3 = "INSERT INTO users_in_guilds(guild_id, user_id) VALUES ($1, $2)"
+    @property
+    async def user_bans(self) -> list:
+        return [x[0] for x in await self.pdb.fetch("SELECT id FROM users where banned = true")]
 
-        for guild in self.guilds:
-            try:
-                await self.pdb.execute(query, guild.id)
-            except asyncpg.UniqueViolationError:
-                pass
+    @property
+    async def guild_bans(self) -> list:
+        return [x[0] for x in await self.pdb.fetch("SELECT id FROM guilds where banned = true")]
 
-            for member in guild.members:
-                try:
-                    await self.pdb.execute(query2, member.id, member.name, int(member.discriminator))
-                except asyncpg.UniqueViolationError:
-                    pass
-                try:
-                    await self.pdb.execute(query3, guild.id, member.id)
-                except asyncpg.UniqueViolationError:
-                    pass
+    @property
+    async def bans(self):
+        return [x[0] for x in await self.pdb.fetch(
+            "SELECT id FROM users where banned = true UNION SELECT id FROM guilds where banned = true")]
+
+    async def prefixess(self, guild_id):
+        return await self.prefixes.smembers(f'{self.instance_name}_{guild_id}')
 
     async def on_ready(self):
         """
@@ -132,8 +114,8 @@ class Nya_Nya(commands.AutoShardedBot):
         print("[*] CONNECTED")
 
     async def on_message_edit(self, before, after):
-        if before.content != after.content and self._blacklist(
-                after):  # prevents double invocation ex.: (when u send spotify url it edits message and adds embed)
+        if before.content != after.content and self._blacklist(after):
+            # prevents double invocation ex.: (when u send spotify url it edits message and adds embed)
             await self.process_commands(after)
 
     async def run(self):
@@ -155,26 +137,12 @@ class Nya_Nya(commands.AutoShardedBot):
         """
         return await super().get_context(message, cls=cls or NyaNyaContext)
 
-    @property
-    async def user_bans(self) -> list:
-        return [x[0] for x in await self.pdb.fetch("SELECT id FROM users where banned = true")]
-
-    @property
-    async def guild_bans(self) -> list:
-        return [x[0] for x in await self.pdb.fetch("SELECT id FROM guilds where banned = true")]
-
-    @property
-    async def bans(self):
-        return (await self.user_bans).extend((await self.guild_bans))
-
     async def _blacklist(self, ctx):
         """
         Check if user is blacklisted.
         """
-        bans = [x[0] for x in await self.pdb.fetch(
-            "SELECT id FROM users where banned = true UNION SELECT id FROM guilds where banned = true")]
-
-        if (ctx.author.id in bans or ctx.guild.id in bans) and not ctx.author.id in self.owner_ids:
+        bans = await self.bans
+        if (ctx.author.id in bans or ctx.guild.id in bans) and ctx.author.id not in self.owner_ids:
             return False
         else:
             return True
@@ -190,8 +158,8 @@ class Nya_Nya(commands.AutoShardedBot):
         if msg.guild:
             try:
                 async with async_timeout.timeout(0.05):  # 50ms normal query takes about 1ms
-                    guild_prefixes = await self.prefixes.smembers(f'{self.instance_name}_{msg.guild.id}')
-            except Exception:
+                    guild_prefixes = await self.prefixess(msg.guild.id)
+            except asyncio.TimeoutError:
                 guild_prefixes = ()
         else:
             guild_prefixes = ()
@@ -204,6 +172,34 @@ class Nya_Nya(commands.AutoShardedBot):
     async def remove_prefix(self, ctx, prefix):
         await self.prefixes.srem(f'{self.instance_name}_{ctx.guild.id}', prefix)
 
+    def load_webhook(self):
+        a = discord.AsyncWebhookAdapter(self.session)
+        try:
+            self.error_webhook = discord.Webhook.from_url(self.cfg.ERROR_WEBHOOK_URL, adapter=a)
+        except Exception:
+            ...
+        try:
+            self.report_webhook = discord.Webhook.from_url(self.cfg.REPORT_WEBHOOK_URL, adapter=a)
+        except Exception:
+            ...
 
-class NyaCog(commands.Cog):
-    ...
+    async def log_to_db(self):
+        query = "INSERT INTO guilds(id) VALUES ($1)"
+        query2 = "INSERT INTO users(id, name, discriminator) VALUES ($1, $2, $3)"
+        query3 = "INSERT INTO users_in_guilds(guild_id, user_id) VALUES ($1, $2)"
+
+        for guild in self.guilds:
+            try:
+                await self.pdb.execute(query, guild.id)
+            except asyncpg.UniqueViolationError:
+                pass
+
+            for member in guild.members:
+                try:
+                    await self.pdb.execute(query2, member.id, member.name, int(member.discriminator))
+                except asyncpg.UniqueViolationError:
+                    pass
+                try:
+                    await self.pdb.execute(query3, guild.id, member.id)
+                except asyncpg.UniqueViolationError:
+                    pass
